e62052b3f7ef623f4835a052a22e50ead5b00ea5
dan01jan <danize1001@gmail.com>
2024-02-07 16:57:41 +0800

    First Initial Commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import Header from './Shared/Header';
+import { NativeBaseProvider, extendTheme, } from "native-base";
+import { NavigationContainer } from '@react-navigation/native'
+import Main from './Navigators/Main';
+
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+import Toast from "react-native-toast-message";
+
+const theme = extendTheme({ colors: newColorTheme });
+const newColorTheme = {
+  brand: {
+    900: "#8287af",
+    800: "#7c83db",
+    700: "#b3bef6",
+  },
+};
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NativeBaseProvider theme={theme}>
+        <NavigationContainer>
+          <Header />
+          <Main />
+          <Toast />
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </Provider>
+  );
+}
